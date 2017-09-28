5d094d68e26ef6579e4a2bf27be02ed422ecf293
Zavier Aguila <zavieraguila@gmail.com>
2017-09-27 20:17:50 -0700

    Added initial Xcode project shell & gitignore
    Xcode project has the pods: Alamofire and SwiftyJSON added.

---
+++ Prep/Pods/Target Support Files/Pods-PrepUITests/Pods-PrepUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PrepUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PrepUITests
+@end
---
+++ Prep/Prep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prep
+//
+//  Created by Zavier Patrick David Aguila on 9/27/17.
+//  Copyright © 2017 Zavier Patrick David Aguila. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
