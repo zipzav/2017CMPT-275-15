54f5ae6450e7a682c919d15f9e9a74c09f6be6ee
Zavier Aguila <zavieraguila@gmail.com>
2017-10-28 11:33:52 -0700

    Added Base Experience Class.

---
+++ Prep/Prep/ExperienceObject.swift
@@ -0,0 +1,47 @@
+//
+//  ExperienceObject.swift
+//  Prep
+//
+//  Created by Zavier Patrick David Aguila on 10/28/17.
+//  Copyright © 2017 Zavier Patrick David Aguila. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class Experience{
+    var name:String
+    var description:String
+    var panoramas: [Panorama] //Each Experience has an Array to hold all its Panorama
+    
+    init (Name:String, Description:String){
+        name = Name
+        description = Description
+        panoramas = [Panorama]()//Empty
+    }
+
+}
+
+class Location{
+    var x: Int
+    var y: Int
+    
+    init(X: Int, Y:Int){
+        x = X
+        y = Y
+    }
+}
+class Panorama{
+    var image:UIImage
+    var buttonLocation: [Location] //the index between buttonLocation and Button object should match
+    var buttonObject : [Any]
+    var buttonsPressed : Int //A counter? You could probably check how many buttons there are in buttonObect Array and compare against this value
+    
+    init(Image:UIImage){
+        image = Image
+        buttonsPressed = 0
+        buttonLocation = [Location]()
+        buttonObject = [Any]()
+    }
+}
